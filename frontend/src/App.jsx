2465c3495e565ea3e4f61f9eae8f1c1e6bcde9bd
scubar <daniel.j.elps@gmail.com>
2026-02-15 20:54:37 -0500

    FIx build failure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,39 +29,32 @@ function ProtectedRoute({ children }) {
   return children
 }
 
-function AppContent() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-dk-1 flex flex-col pb-16">
-        <Header />
-        <main className="container mx-auto px-4 py-8 flex-1">
-          <ApiErrorProvider>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<ProtectedRoute><SearchPage /></ProtectedRoute>} />
-            <Route path="/set/:setNum" element={<ProtectedRoute><SetDetailPage /></ProtectedRoute>} />
-            <Route path="/projects" element={<ProtectedRoute><ProjectsPage /></ProtectedRoute>} />
-            <Route path="/projects/:projectId" element={<ProtectedRoute><ProjectDetailPage /></ProtectedRoute>} />
-            <Route path="/settings" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
-            <Route path="/settings/cache" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
-            <Route path="/settings/database" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
-            <Route path="/guide" element={<ProtectedRoute><DocumentationPage /></ProtectedRoute>} />
-            <Route path="/attributions" element={<ProtectedRoute><AttributionsPage /></ProtectedRoute>} />
-          </Routes>
-        </ApiErrorProvider>
-      </main>
-      <footer className="border-t border-dk-3 bg-dk-2 mt-auto py-3 px-4 text-center text-sm text-dk-5">
-        This project is not compatible with LEGO&trade;. LEGO&trade; is a trademark of the LEGO Group, which does not sponsor, authorize or endorse this project.
-      </footer>
-    </div>
-  )
-}
-
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <AppContent />
+        <div className="min-h-screen bg-dk-1 flex flex-col pb-16">
+          <Header />
+          <main className="container mx-auto px-4 py-8 flex-1">
+            <ApiErrorProvider>
+              <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<ProtectedRoute><SearchPage /></ProtectedRoute>} />
+                <Route path="/set/:setNum" element={<ProtectedRoute><SetDetailPage /></ProtectedRoute>} />
+                <Route path="/projects" element={<ProtectedRoute><ProjectsPage /></ProtectedRoute>} />
+                <Route path="/projects/:projectId" element={<ProtectedRoute><ProjectDetailPage /></ProtectedRoute>} />
+                <Route path="/settings" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
+                <Route path="/settings/cache" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
+                <Route path="/settings/database" element={<ProtectedRoute><SettingsPage /></ProtectedRoute>} />
+                <Route path="/guide" element={<ProtectedRoute><DocumentationPage /></ProtectedRoute>} />
+                <Route path="/attributions" element={<ProtectedRoute><AttributionsPage /></ProtectedRoute>} />
+              </Routes>
+            </ApiErrorProvider>
+          </main>
+          <footer className="border-t border-dk-3 bg-dk-2 mt-auto py-3 px-4 text-center text-sm text-dk-5">
+            This project is not compatible with LEGO&trade;. LEGO&trade; is a trademark of the LEGO Group, which does not sponsor, authorize or endorse this project.
+          </footer>
+        </div>
       </AuthProvider>
     </Router>
   )
